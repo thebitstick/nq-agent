0a89a935269596865921b83d38bcf7b73c7b3f0f
Bitstick <thebitstick@tfwno.gf>
2016-10-22 07:24:30 -0500

    Update nq-install.sh

--- nq-install.sh
+++ nq-install.sh
@@ -20,51 +20,11 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # Prepare output
 echo -e "|\n|   NodeQuery BSD Installer\n|   ===================\n|"
 
-# Root required
-if [ $(id -u) != "0" ];
-then
-	echo -e "|   Error: You need to be root to install the NodeQuery agent\n|"
-	echo -e "|          The agent itself will NOT be running as root but instead under its own non-privileged user\n|"
-	exit 1
-fi
-
 # Parameters required
 if [ $# -lt 1 ]
 then
 	echo -e "|   Usage: bash $0 'token'\n|"
 	exit 1
-fi
-
-# Check if crontab is installed
-if [ ! -n "$(command -v crontab)" ]
-then
-	echo -e "|\n|   Error: Crontab is required \n|"
-	exit 1
-fi
-
-# Check if cron is running
-if [ -z "$(ps -Al | grep cron | grep -v grep)" ]
-then
-	
-	# Confirm cron service
-	echo "|" && read -p "|   Cron is available but not running. Do you want to start it? [Y/n] " input_variable_service
-
-	# Attempt to start cron
-	if [ -z $input_variable_service ] || [ $input_variable_service == "Y" ] || [ $input_variable_service == "y" ]
-	then
-		if [ -n "$(command -v pkg)" ]
-		then
-			echo -e "|\n|   Notice: Starting 'cron' via 'service'"
-			service cron onestart
-	fi
-	
-	# Check if cron was started
-	if [ -z "$(ps -Al | grep cron)" ]
-	then
-		# Show error
-		echo -e "|\n|   Error: Cron is available but could not be started\n|"
-		exit 1
-	fi
 fi
 
 # Attempt to delete previous agent
@@ -108,5 +68,4 @@ then
 else
 	# Show error
 	echo -e "|\n|   Error: The NodeQuery agent could not be installed\n|"
-fi
 fi
